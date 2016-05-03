d150daeaf83af4f71bddb5675239cdc0e86187e4
bartonunes <bartonunes@hotmail.com>
2016-05-03 20:47:45 -0300

    arquivo alterado no dia 0305

--- src/pacote1/Classe1.java
+++ src/pacote1/Classe1.java
@@ -4,7 +4,16 @@ public class Classe1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
+		System.out.println("ola negada");
+	
+	for(int i=0; i<10;i++){
+		for( int j=0;j<=10;j++){
+			System.out.println("*");
+		}
+		System.out.println();
+	}
+		System.err.println("\n");
 	}
 
 }
